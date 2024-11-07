88aaa91cc0a02da39a951bcbea9a919165d7f68b
hlan-1920 <khquynh205@gmail.com>
2024-11-07 22:12:02 +0700

    first commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+console.log("Certification page loaded");
+// Add any dynamic features if needed, e.g., expand/collapse sections
+
+function redirectToPost() {
+    window.location.href = "link-to-your-post.html"; // Thay thế bằng link đến bài viết của bạn
+}
+
+function navigateTo(url) {
+    window.location.href = url;
+}
